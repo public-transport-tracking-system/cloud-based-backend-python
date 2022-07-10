54ab194cbd2917a2266bfba4dd062c313bc8b6cc
Daniel Klinkert <daniel.klinkert@gmail.com>
2022-07-10 13:25:47 +0200

    update server to allow publishing back to stations

--- server.py
+++ server.py
@@ -15,9 +15,14 @@ context = zmq.Context()
 responder = context.socket(zmq.REP)
 responder.bind("tcp://*:5555")
 logging.info("Waiting for client")
+
 subscriber = context.socket(zmq.SUB)
 subscriber.bind("tcp://*:5556")
 subscriber.connect("tcp://127.0.0.1:5556")
+
+publisher = context.socket(zmq.PUB)
+publisher.bind("tcp://*:5557")
+publisher.connect("tcp://127.0.0.1:5557")
 
 # subscribe to route 1 updates
 subscriber.setsockopt(zmq.SUBSCRIBE, b"1")
@@ -28,12 +33,13 @@ poller.register(responder, zmq.POLLIN)
 poller.register(subscriber, zmq.POLLIN)
 
 dataFromClient = {}
+stationsToSendInfo = {}
 
 table = PrettyTable()
 table.field_names = ["Route", "DataSent", "Station"]
 
 def displayData(dataFromClient):
-    os.system('clear')
+    #os.system('clear')
     table.clear_rows()
     for key, value in dataFromClient.items():
         number = len(value)
@@ -57,16 +63,25 @@ while True:
         break
 
     if responder in socks:
-        message = responder.recv()
+        route_id, bus_id = responder.recv_string().split("/")
         # simulate latency
         time.sleep(2)
-        responder.send(message)
+        if not route_id in stationsToSendInfo:
+            stationsToSendInfo[route_id] = set([bus_id])
+        else:
+            existingSet = stationsToSendInfo[route_id]
+            existingSet.add(bus_id)
+            stationsToSendInfo[route_id] = existingSet
+        
+        print(stationsToSendInfo)
+
+        responder.send_string(bus_id)
 
     if subscriber in socks:
         message = subscriber.recv_string()
         data = message.split("/")
-        routeInfo = json.loads(data[1], object_hook=customRouteDecoder)
-        for route in routeInfo:
+        stationInfo = json.loads(data[1], object_hook=customRouteDecoder)
+        for route in stationInfo:
             currentRoute = route.dataFromSensor.bus_id
             if currentRoute in dataFromClient:
                 pending = dataFromClient.get(currentRoute)
@@ -74,3 +89,10 @@ while True:
                 dataFromClient[currentRoute] = pending
             else:
                 dataFromClient[currentRoute] = [route]
+
+            for key, subscribedRoutes in stationsToSendInfo.items():
+                el_in_set = route.dataFromSensor.bus_id in subscribedRoutes
+                if el_in_set:
+                    print(f"sending info to {key}, for route {route.dataFromSensor.bus_id}")
+                    #send information to other stations that are listening for the bus id for simplicity only send current time
+                    publisher.send_string(f"{key}/{route.dataFromSensor.bus_id}/{int(time.time())}")
